070ef860b15f1901683dbb929bcc24f72a204a8a
Dominik Tilp <Dominik.Tilp@dixonscarphone.com>
2019-07-23 09:30:52 +0200

    add Game class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import next from './gol/gol';
+import Game from './gol/game';
 
-next([]);
+const state = [
+  [0, 0, 0, 1, 1],
+  [0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1],
+  [0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 1],
+];
+
+const game = new Game(state);
+game.next();
+console.log('1', game.getBoard());
+console.log(game.isAlive());
+game.next();
+console.log('2', game.getBoard());
+console.log(game.isAlive());
+game.next();
+console.log('3', game.getBoard());
+console.log(game.isAlive());
+game.next();
+console.log('4', game.getBoard());
+console.log(game.isAlive());
+game.next();
+console.log('5', game.getBoard());
+console.log(game.isAlive());
+game.next();
+console.log('6', game.getBoard());
+console.log(game.isAlive());
+game.next();
