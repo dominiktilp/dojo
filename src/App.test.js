0297e2b40a8521988722c7fa3664c6a4c4cb91e1
Dominik Tilp <Dominik.Tilp@dixonscarphone.com>
2019-07-23 10:25:00 +0200

    init react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { render } from '@testing-library/react';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders header', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Game of Life')).toBeInTheDocument();
 });
